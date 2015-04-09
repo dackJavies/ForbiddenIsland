c2a5579f3bdb12182ee4a342d01976e0686b9edd
Alexander Cherry <acherry125@gmail.com>
2015-04-08 20:12:28 -0400

    gjjh

--- IslandGame.java
+++ IslandGame.java
@@ -509,7 +509,6 @@ class ForbiddenIslandWorld extends World {
         this.chopper = new HelicopterTarget(this.findValidLoc(), this.pieces);*/
         
     }
-    
     // Creates a standard map
     IList<Cell> makeMountain(boolean isRandom) {
 
@@ -675,6 +674,7 @@ class ForbiddenIslandWorld extends World {
         this.waterHeight += 1;
 
     }
+<<<<<<< HEAD
     
     Cell findValidLoc() {
         
@@ -714,6 +714,8 @@ class FindValidLoc implements IVisitor<Cell, Cell> {
         throw new IllegalArgumentException("IBST is not a valid argument");
     }
     
+=======
+>>>>>>> parent of fc86fb2... pewp dixxx
 }
 
 // represent the player's avatar: the pilot
@@ -772,14 +774,11 @@ class Player {
 
     // Can the player move in the given direction?
     boolean safe(String dir) {
+
         return this.location.safeHelp(dir);
+
+
     }
-    
-    // Is the player dead?
-    boolean isDead() {
-        return this.location.isFlooded;
-    }
-    
 }
 
 // represents objects the player needs to obtain
@@ -837,16 +836,10 @@ class HelicopterTarget extends Target {
             return this.pieces.accept(new TargetListVisitor(p.inventory));
         }
     }
-    
-    // Did the given Player win?
-    boolean win(Player p) {
-        return this.touching(p.location.x, p.location.y) &&
-                this.canBeRepaired(p);
-    }
 
 }
 
-// Goes through a list of Targets, seeing if each one is in the given 2nd list
+//Goes through a list of Targets, seeing if each one is in the given 2nd list
 class TargetListVisitor implements IVisitor<Target, Boolean> {
 
     IList<Target> src;
@@ -874,7 +867,7 @@ class TargetListVisitor implements IVisitor<Target, Boolean> {
     }
 }
 
-// Is the given Target in a list of Targets?
+//Is the given Target in a list of Targets?
 class TargetVisitor implements IVisitor<Target, Boolean> {
 
     Target toFind;
