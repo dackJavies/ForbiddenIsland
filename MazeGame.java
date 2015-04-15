06ae26862ace8f051412da2c5b6cff711a38c06c
Alexander Cherry <acherry125@gmail.com>
2015-04-15 14:58:16 -0400

    Map done

--- MazeGame.java
+++ MazeGame.java
@@ -67,6 +67,148 @@ class Mt<T> implements IList<T> {
     }
 }
 
+//represents the deque collection of items
+class Deque<T> {
+  Sentinel<T> header;
+  // initializes the deque with a new Sentinel
+  Deque() {
+      this.header = new Sentinel<T>();
+  }
+  // initializes the deque with the given Sentinel
+  Deque(Sentinel<T> header) {
+      this.header = header;
+  }
+  // counts the number of nodes in this deque
+  int size() {
+      return this.header.next.countNodes();
+  }
+  // EFFECTS: Mutates the header and first item of the Deque's prev and next field
+  // adds a node to the beginning of the deque
+  void addAtHead(T t) {
+      new Node<T>(t, this.header.next, this.header);
+  }
+  // EFFECTS: Mutates the header and last item of the Deque's prev and next field
+  // adds a node to the beginning of the deque
+  void addAtTail(T t) {
+      new Node<T>(t, this.header, this.header.prev);
+  }
+  // EFFECTS: Mutates the header and first item of the Deque's prev and next field
+  // adds a node to the beginning of the deque
+  T removeFromHead() {
+      if (!this.header.next.isNode()) {
+          throw new RuntimeException("cannot remove first item from empty list");
+      }
+      else {
+          T temp = ((Node<T>)(this.header.next)).data;
+          this.header.next = this.header.next.next;
+          this.header.next.prev = this.header;
+          return temp;
+      }
+  }
+  // EFFECTS: Mutates the header and last item of the Deque's prev and next field
+  // adds a node to the beginning of the deque
+  T removeFromTail() {
+      if (!this.header.prev.isNode()) {
+          throw new RuntimeException("cannot remove last item from empty list");
+      }
+      else {
+          T temp = ((Node<T>)(this.header.prev)).data;
+          this.header.prev = this.header.prev.prev;
+          this.header.prev.next = this.header;
+          return temp;
+      }
+  }
+  // removes the given node from the deque
+  void removeNode(ANode<T> n) {
+      if (n.isNode()) {
+          n.prev.next = n.next;
+          n.next.prev = n.prev;
+      }
+  }
+}
+
+//represents a node in a deck
+abstract class ANode<T> {
+  ANode<T> next;
+  ANode<T> prev;
+  ANode(ANode<T> next, ANode<T> prev) {
+      this.next = next;
+      this.prev = prev;
+  }
+  // EFFECTS: Mutates the prev or next field
+  // updates this node with a new prev or next node
+  void updateSelf(ANode<T> n, boolean isPrev) {
+      if (isPrev) {
+          this.prev = n;
+      }
+      else {
+          this.next = n;
+      }
+  }
+  // determines whether this node is a non-header node
+  boolean isNode() {
+      return true;
+  }
+  // counts how many nodes come after this one inclusively
+  int countNodes() {
+      return 1 + this.next.countNodes();
+  }
+}
+
+//represents the header node of a deque
+class Sentinel<T> extends ANode<T> {
+  Sentinel() {
+      super(null, null);
+      this.next = this;
+      this.prev = this;
+  }
+  // updates this sentinel with a new prev or next node
+  void updateSelf(ANode<T> n, boolean isPrev) {
+      if (isPrev) {
+          this.prev = n;
+      }
+      else {
+          this.next = n;
+      }
+  }
+  // determines that this is not a non-header node
+  boolean isNode() {
+      return false;
+  }
+  // counts how many nodes come after this one inclusively
+  int countNodes() {
+      return 0;
+  } 
+}
+
+//represents a non-header node of a deque
+class Node<T> extends ANode<T> {
+  T data;
+  // creates a node with no connecting nodes
+  Node(T data) {
+      super(null, null);
+      this.data = data;
+  }
+  // EFFECTS: Mutates the prev and next's prev and next field
+  // creates a node with connecting nodes
+  Node(T data, ANode<T> next, ANode<T> prev) {
+      super(null, null);
+      this.data = data;
+      if (next == null) {
+          throw new IllegalArgumentException("next node cannot be null");
+      }
+      else if (prev == null) {
+          throw new IllegalArgumentException("prev node cannot be null");
+      }
+      else {
+          this.next = next;
+          this.prev = prev;
+          this.next.updateSelf(this, true);
+          this.prev.updateSelf(this, false);
+      }
+  }
+}
+
 //represents a Stack
 //Used for Depth First Search
 class Stack<T> {
